abada3b7f5420dbe4cf33c4342ab9ecffa315fbb
Orlando Flores <orlando_flores@id.uff.br>
2021-08-19 16:18:52 -0300

    feat: log the movies list in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './App.css';
+import tmdb from './services/api'
 
 function App() {
+
+  useEffect(() => {
+    const fetchHomeList = async () => {
+      const list = await tmdb();
+      console.log(list);
+    }
+
+    fetchHomeList();
+  }, [])
+
   return (
     <div className="App">
       Hello world!
