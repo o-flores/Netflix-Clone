147131c7c3e326ef876c278542eacd58e71ef796
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 18:23:34 -0300

    feat: create navigation icons and css

--- src/components/MovieContainer/index.jsx
+++ src/components/MovieContainer/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import MovieCard from '../MovieCard';
+import './style.css';
+import { MdNavigateNext } from 'react-icons/md';
+import { MdNavigateBefore } from 'react-icons/md';
+
 
 function MovieContainer({ title, list }) {
   return (
     <>
       <h2>{ title }</h2>
-      <div>
-        { list.results.map((item) => {
-          return <MovieCard key={ item.id } {...item} />
-        }) }
+      <div className="carousel-container">
+        <MdNavigateBefore className="navigate-prev"/>
+        <div className="movieContainer">
+          { list.results.map((item) => {
+            return <MovieCard key={ item.id } {...item} />
+          }) }
+        </div>
+        <MdNavigateNext className="navigate-next"/>
       </div>
     </>
   );
