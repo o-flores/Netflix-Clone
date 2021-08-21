ab4036056bb18610092922046e29afa12d70f764
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 19:20:54 -0300

    feat: creates carrousel with css

--- src/components/MovieContainer/index.jsx
+++ src/components/MovieContainer/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MovieCard from '../MovieCard';
 import './style.css';
 import { MdNavigateNext } from 'react-icons/md';
@@ -6,17 +6,44 @@ import { MdNavigateBefore } from 'react-icons/md';
 
 
 function MovieContainer({ title, list }) {
+  const [scrollx, setScrollx] = useState(0);
+
+  function handlePrev() {
+    let x = scrollx + Math.round(window.innerWidth / 2);
+    if (x > 0) x = 0;
+    setScrollx(x);
+  }
+
+  function handleNext() {
+    let x = scrollx - Math.round(window.innerWidth / 2);
+    const totalWidth = list.results.length * 200;
+    if ((window.innerWidth - totalWidth) > x) {
+      x = window.innerWidth - totalWidth - 60;
+    }
+    setScrollx(x);
+  }
   return (
     <>
       <h2>{ title }</h2>
-      <div className="carousel-container">
-        <MdNavigateBefore className="navigate-prev"/>
+      <div 
+        className="carousel-container"
+        style={{ marginLeft: scrollx }}
+      >
+        {scrollx !== 0 &&
+          <MdNavigateBefore
+            onClick={ handlePrev }
+            className="navigate-prev"
+          />
+        }
         <div className="movieContainer">
           { list.results.map((item) => {
             return <MovieCard key={ item.id } {...item} />
           }) }
         </div>
-        <MdNavigateNext className="navigate-next"/>
+        <MdNavigateNext
+          onClick={ handleNext }
+          className="navigate-next"
+        />
       </div>
     </>
   );
