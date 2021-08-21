4c34408720af8facd69b7d43dacea55b1916cc27
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 18:22:50 -0300

    feat: create animation on poster

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './style.css';
 
 function MovieCard(props) {
   const BASE_URL = 'http://image.tmdb.org/t/p/';
   const backdropPath = `${BASE_URL}w200${props.poster_path}`
   return (
-    <div>
+    <div className="poster">
       <img
         src={backdropPath}
         alt={`poster of ${props.title}`}
