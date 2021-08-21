a8f91dec81cbb348c8194aa5c72062cf6570915d
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 17:12:30 -0300

    feat: create initial structure of movie cards

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-function MovieCard({ title }) {
+function MovieCard(props) {
+  const BASE_URL = 'http://image.tmdb.org/t/p/';
+  const backdropPath = `${BASE_URL}w200${props.poster_path}`
   return (
-    <h2>{ title }</h2>
+    <div>
+      <img
+        src={backdropPath}
+        alt={`poster of ${props.title}`}
+      />
+    </div>
   );
 };
 
