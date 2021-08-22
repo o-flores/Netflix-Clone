b93ad2b5039827a4a8b7c4e5f1cf9d166d57acae
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 23:14:50 -0300

    feat: add Header and FeaturedMovie components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import MoviesList from './components/MoviesList';
+import Header from './components/Header';
+import FeaturedMovie from './components/FeaturedMovie';
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
+      <FeaturedMovie />
       <MoviesList />
     </div>
   );
