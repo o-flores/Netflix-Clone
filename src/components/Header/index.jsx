1e759a63d0d855d1b2d99d02af7fc81ade3eba7e
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 23:16:07 -0300

    feat: add header structure and css

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from '../../images/netflixLogo.png';
+import './style.css';
+import { MdSearch } from 'react-icons/md';
+import { MdNotificationsNone } from 'react-icons/md';
+import avatar from '../../images/avatar.png';
+
+
+function Header() {
+  return(
+    <div className="header-container">
+      <div className="left-header">
+        <div className="logo">
+          <a href="/">
+            <img src={logo} alt="Netflix Logo" />
+          </a>
+        </div>
+        <a href="/">Início</a>
+        <a href="/">Séries</a>
+        <a href="/">Filmes</a>
+        <a href="/">Bombando</a>
+        <a href="/">Minha Lista</a>
+      </div>
+      <div className="right-header">
+        <MdSearch className="search-icon" />
+        <MdNotificationsNone className="notification-icon" />
+        <img src={ avatar } alt="Netflix Avatar" />
+      </div>
+
+    </div>
+  );
+}
+
+export default Header;
