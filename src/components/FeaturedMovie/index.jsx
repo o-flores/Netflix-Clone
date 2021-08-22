ba1c3eaf388431c2885726033128506623dc2706
Orlando Flores <orlando_flores@id.uff.br>
2021-08-21 23:16:29 -0300

    feat: first structure of FeaturedMovie component

---
+++ src/components/FeaturedMovie/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function FeaturedMovie() {
+  return(
+    <div className="featured">
+      Filmao
+    </div>
+  );
+}
+
+export default FeaturedMovie;
