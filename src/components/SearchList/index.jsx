1312752a41f07660c3005e7d30bcbddd06c4e582
Orlando Flores <orlando_flores@id.uff.br>
2021-08-23 19:34:26 -0300

    feat: renders movies cards

--- src/components/SearchList/index.jsx
+++ src/components/SearchList/index.jsx
@@ -1,12 +1,23 @@
 import React, { useContext } from 'react';
 import { SearchContext } from '../../Contexts/SearchText';
+import MovieCard from '../MovieCard';
+import './style.css';
 
 function SearchList() {
   const { queryResults } = useContext(SearchContext);
 
+  let queryResultsFilter = queryResults.results;
+  if (queryResults.results) {
+    queryResultsFilter = queryResults.results.filter((result) => result.media_type !== 'person' && result.poster_path !== null);
+  }
+
   return (
     <div className="list-container">
-      Filmes
+      {queryResultsFilter && queryResultsFilter.length > 0 && queryResultsFilter.map((result) => {
+        return <MovieCard key={ result.id } {...result} />
+      })
+      }
+      { queryResultsFilter && queryResultsFilter.length === 0 && <p>sem resultados</p>}
     </div>
   );
 };
