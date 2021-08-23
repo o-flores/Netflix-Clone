0e5c045ec4ac1a5ecfa310faf73125d7500869e0
Orlando Flores <orlando_flores@id.uff.br>
2021-08-22 23:00:37 -0300

    feat: structure and css for the featured movie

--- src/components/FeaturedMovie/index.jsx
+++ src/components/FeaturedMovie/index.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchMoviesOrTvInfo, BASE_URL, API_KEY } from '../../services/api';
 import './style.css';
+import { FaPlay } from "react-icons/fa";
+import { FaInfo } from "react-icons/fa";
+
 
 function FeaturedMovie() {
-  return(
-    <div className="featured">
-      Filmao
+
+  const [featuredTv, setfeaturedTv] = useState([]);
+  const [featuredTvInfo, setfeaturedTvInfo] = useState([]);
+
+
+  useEffect(() => {
+    const numberOfInfoPerPage = 20;
+    const fetchFeaturedSeries = async () => {
+      const { results } = await fetchMoviesOrTvInfo(`${BASE_URL}/discover/tv?api_key=${API_KEY}&with_watch_providers=8&watch_region=BR&sort_by=popularity.desc&language=pt-BR`);
+      const randomNumber = Math.round(Math.random() * numberOfInfoPerPage);
+      setfeaturedTv(results[randomNumber]);
+    }
+    fetchFeaturedSeries();
+  }, []);
+
+  useEffect(() => {
+    const fetchDetailsTvInfo = async () => {
+      const info = await fetchMoviesOrTvInfo(`${BASE_URL}/tv/${featuredTv.id}?api_key=${API_KEY}&language=pt-BR`);
+      setfeaturedTvInfo(info);
+    }
+    if (featuredTv.length !== 0) fetchDetailsTvInfo();
+  }, [featuredTv]);
+
+  console.log(featuredTv);
+
+  return (
+    <div
+      className="featured"
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: `url(http://image.tmdb.org/t/p/original/${featuredTv.backdrop_path})`
+      }}
+    >
+      <div className="vertical-gradient">
+        <div className="horizontal-gradient">
+          <div className="infos">
+            <div className="title">
+              <h1>{featuredTv.original_name}</h1>
+            </div>
+            <div className="overview">
+              <p>{featuredTvInfo.overview}</p>
+            </div>
+            <div className="buttons">
+              <button className="play-btn">
+                <FaPlay className="icon" />
+                Assistir
+              </button>
+              <button className="info-btn">
+                <FaInfo className="icon" />
+                Mais informações
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
