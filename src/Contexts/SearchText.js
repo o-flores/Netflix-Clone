88cfebb6bbe68854541862283551fa0513d7b847
Orlando Flores <orlando_flores@id.uff.br>
2021-08-23 18:41:03 -0300

    feat: add query results to context

--- src/Contexts/SearchText.js
+++ src/Contexts/SearchText.js
@@ -1,12 +1,23 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
+import { searchMulti } from '../services/api';
 
 export const SearchContext = createContext({});
 
 export const SearchContextProvider = ({ children }) => {
   const [searchText, setSearchText] = useState('');
+  const [queryResults, setQueryResults] = useState([]);
+
+  useEffect(() => {
+    const fetchMoviesByQuery = async () => {
+      const data = await searchMulti(searchText);
+      setQueryResults(data);
+    }
+    fetchMoviesByQuery();
+  }, [searchText]);
+
   return (
     <SearchContext.Provider
-      value={ {searchText, setSearchText} }
+      value={ {searchText, setSearchText, queryResults} }
     >
       { children }
     </SearchContext.Provider>
