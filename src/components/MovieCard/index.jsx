c16d9fe729a8ceb7913fc27966990df630bc85ff
Orlando Flores <orlando_flores@id.uff.br>
2021-08-23 19:33:59 -0300

    fix: variables names

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './style.css';
 
-function MovieCard(props) {
+function MovieCard({ title, poster_path }) {
   const BASE_URL = 'http://image.tmdb.org/t/p/';
-  const posterPath = `${BASE_URL}w154${props.poster_path}`
+  const posterPath = `${BASE_URL}w154${poster_path}`;
   return (
     <div className="poster">
       <img
         src={posterPath}
-        alt={`poster of ${props.title}`}
+        alt={`poster of ${title}`}
       />
     </div>
   );
