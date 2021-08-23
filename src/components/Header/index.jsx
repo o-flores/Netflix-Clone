8c267385ecedae4666a9d47da2b058eca5c6c7bf
Orlando Flores <orlando_flores@id.uff.br>
2021-08-22 23:01:03 -0300

    fix: tag name and background color for header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,26 +8,27 @@ import avatar from '../../images/avatar.png';
 
 function Header() {
   return(
-    <div className="header-container">
-      <div className="left-header">
-        <div className="logo">
-          <a href="/">
-            <img src={logo} alt="Netflix Logo" />
-          </a>
+    <header>
+      <div className="header-container">
+        <div className="left-header">
+          <div className="logo">
+            <a href="/">
+              <img src={logo} alt="Netflix Logo" />
+            </a>
+          </div>
+          <a href="/">Início</a>
+          <a href="/">Séries</a>
+          <a href="/">Filmes</a>
+          <a href="/">Bombando</a>
+          <a href="/">Minha Lista</a>
         </div>
-        <a href="/">Início</a>
-        <a href="/">Séries</a>
-        <a href="/">Filmes</a>
-        <a href="/">Bombando</a>
-        <a href="/">Minha Lista</a>
+        <div className="right-header">
+          <MdSearch className="search-icon" />
+          <MdNotificationsNone className="notification-icon" />
+          <img src={ avatar } alt="Netflix Avatar" />
+        </div>
       </div>
-      <div className="right-header">
-        <MdSearch className="search-icon" />
-        <MdNotificationsNone className="notification-icon" />
-        <img src={ avatar } alt="Netflix Avatar" />
-      </div>
-
-    </div>
+    </header>
   );
 }
 
